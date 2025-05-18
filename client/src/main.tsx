563096d01f46669137e86e99c0234a198b6d8ebc
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-05-18 19:11:06 -0400

    hotfix
    forgot to push this with the last push

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,6 @@ import './styles/main.scss';
 import './styles/login.scss';
 import './styles/inputs.scss';
 
-//remove before production
-import './styles/testingStyles.scss';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
