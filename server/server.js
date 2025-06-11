2a47bb233c16cec84a0955edc3a7ae3cd03b7b12
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-06-10 22:42:10 -0400

    cors origin controls update

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 // define cors settings
 const corsOptions = {
    origin: function (origin, callback) {
-      if (origin.startsWith(process.env.FRONTEND_URL)) {
+      if (origin === process.env.FRONTEND_URL) {
          callback(null, true);
       } else {
          callback(new Error('Not allowed by CORS'));
