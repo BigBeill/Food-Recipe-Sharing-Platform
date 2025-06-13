4973caf5914c60b16d1f73226cbbc56054a60d2f
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-06-13 02:09:25 -0400

    loading page upgrade
    added some more content to the loading page so hopefully people don't get upset at how long the initial load is

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,7 +1,11 @@
 export default function Loading() {
     return(
-        <div>
+        <div className="loadingContainer">
             <p>Loading...</p>
+            <p>
+                If its been over 15 minutes since you last accessed this site the <br />
+                server may have gone into sleep mode. Give it a moment to wake up.
+            </p>
         </div>
     )
 }
